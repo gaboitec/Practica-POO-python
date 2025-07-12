43efb623f8d407a6f88d51e5ac0ef4de2a69bca5
Yefry García <caxajgabriel2@gmail.com>
2025-07-11 23:18:27 -0600

    Finalizacion del main.py con opciones y acciones conpletas

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def menu():
     print("2. Mostrar a todos los estudiantes")
     print("3. Buscar a un estudiante por carné")
     print("4. Calcular el promedio de notas de todos los estudiantes")
+    print("0. SALIR")
 
 while True:
     menu()
@@ -16,8 +17,17 @@ while True:
         listaEstudiantes.agregar_estudiante(nuevo_estudiante)
     elif opcion == "2":
         print("\nESTUDIANTES REGISTRADOS:")
-        listaEstudiantes.get_lista()
+        listaEstudiantes.mostrar_lista()
     elif opcion == "3":
         print("\nBUSCAR ESTUDIANTE:")
         carneIn = input("Ingrese el carné a buscar: ")
-
+        
+        estudiante_encontrado = listaEstudiantes.buscar(carneIn)
+        estudiante_encontrado.get_estudiante()
+    elif opcion == "4":
+        print("\nPROMEDIO DE TODAS LAS NOTAS:")
+        print(listaEstudiantes.get_promedio)
+    elif opcion == "0":
+        break
+    else:
+        print("\nOpción no válida")
