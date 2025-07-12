eac4a848a50322b9433fca1e7fce88d90c4d4abd
ALUMNO <caxajgabriel2@gmail.com>
2025-07-11 21:08:14 -0600

    Creacion de clase para un objeto lista el cual contiene una lista que almacena los objetos tipo estudiante

--- Clases.py
+++ Clases.py
@@ -7,3 +7,14 @@ class Estudiante:
 
     def get_estudiante(self):
         return f"Nombre: {self.__nombre} Carne: {self.__carne} Carrera: {self.__carrera} Nota final: {self.__nota_final}"
+
+class ListaEstudiantes:
+    def __init__(self):
+        self.lista = []
+
+    def agregar_estudiante(self, estudiante: Estudiante):
+        self.lista.append(estudiante)
+
+    def get_estudiantes(self):
+        for estudiante in self.lista:
+            print(estudiante.get_estudiante())
